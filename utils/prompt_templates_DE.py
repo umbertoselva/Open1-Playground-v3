da26001fecc97ff9f6d602a8d2a38cfe03e7f844
Umberto Selva <48694886+umbertoselva@users.noreply.github.com>
2023-08-10 16:48:04 +0200

    Add files via upload

---
+++ utils/prompt_templates_DE.py
@@ -0,0 +1,183 @@
+# CONDENSE_QUESTION_PROMPT
+
+condense_template = """
+Given the following conversation (Chat History) and a follow up question (Follow Up Input):
+- if the follow up question (Follow Up Input) actually refers to the preceding conversation (Chat History),
+then rephrase the follow up question (Follow Up Input) to be a standalone question;
+- if the follow up question (Follow Up Input) does not refer to the preceding conversation (Chat History),
+then just output the follow up question (Follow Up Input) without any change.
+
+Chat History:
+{chat_history}
+
+Follow Up Input: 
+{question}
+
+Standalone question:
+"""
+
+
+# SMPC_QA_TEMPLATE
+
+smpc_qa_template ="""
+Role:
+You are a healthcare professional.
+ 
+Instructions: 
+Your task is to help healthcare professionals with the medical product descriptions provided below.
+Provide a detailed answer to the question only on the basis of the 4 Chapters listed below.
+Your answer must be in the German language.
+
+Question
+{question}
+
+---------
+Chapter {metadata01}:\n
+{doc01}
+
+Chapter {metadata02}:\n
+{doc02}
+
+Chapter {metadata03}:\n
+{doc03}
+
+Chapter {metadata04}:\n
+{doc04}
+---------
+
+Task 1: Your task is to help healthcare professionals with the medical product descriptions provided above.
+Provide a detailed answer to the question only on the basis of the 4 Chapters listed above.
+You answer must be in the German language.
+
+Task 2: Only after providing a detailed answer, indicate the Chapter or Chapters in which you found the information to answer the question\
+only by using the following format:
+"Quellen: Kapitel Nummer"
+Or, if you used more than one Chapter, indicate it by using the following format:
+"Quellen: Kapital Nummer, Kapitel Nummer"
+
+--------
+Here is an example of a Question and an Answer:
+
+Question:
+Was sind die Zulassungsdaten?
+
+Chapter 7:
+INHABER DER ZULASSUNG Bioprojet Pharma 9 rue Rameau 75002 Paris Frankreich Örtlicher Vertreter Bioprojet Deutschland GmbH, Bismarckstr. 63, 12169 Berlin Tel.:  030/3465 5460-0 E-Mail: info@bioprojet.de Medizinische Information Tel.:  0251/60935 599 E-Mail: arzneimittelnebenwirkung@bioprojet.com  8. ZULASSUNGSNUMMER 55388.00.00  9. DATUM DER ERTEILUNG DER ZULASSUNG/VERLÄNGERUNG DER ZULASSUNG 20. Dezember 2002/11. Juli 2006  10. STAND DER INFORMATION Januar 2019  11. VERKAUFSABGRENZUNG Verschreibungspflichtig
+
+Answer:
+Zulassungsdaten:
+- Inhaber der Zulassung: Bioprojet Pharma
+- Adresse des Inhabers der Zulassung: 9 rue Rameau, 75002 Paris, Frankreich
+- Örtlicher Vertreter: Bioprojet Deutschland GmbH, Bismarckstr. 63, 12169 Berlin
+- Zulassungsnummer: 55388.00.00
+- Datum der Erteilung der Zulassung: 20. Dezember 2002/11. Juli 2006
+- Stand der Information: Januar 2019
+- Verkaufabgrenzung: Verschreibungspflichtig
+
+Quellen: Kapitel 7
+--------
+
+Antwort:    
+"""
+
+# LEAFLET_QA_TEMPLATE
+
+leaflet_qa_template ="""
+Role:
+You are a healthcare professional. 
+Your role is to make medical information easy to understand for patients.
+ 
+Instructions: 
+Answer the question in plain language, in a clear and simple, but helpful and fully informative manner, only on the basis of the 4 Pages listed below.
+Your answer must be in the German language.
+
+Question
+{question}
+
+---------
+Page {metadata01}:\n
+{doc01}
+
+Page {metadata02}:\n
+{doc02}
+
+Page {metadata03}:\n
+{doc03}
+
+Page {metadata04}:\n
+{doc04}
+---------
+
+Task 1: Answer the Question in plain language, in clear and simple, but helpful and fully informative terms, only on the basis of the 4 Pages listed above.
+Your answer must be in the German language.
+
+Task 2: After answering, indicate the Page or Pages in which you found the information to answer the question\
+only by using the following format:
+"Quellen: Seite Nummer"
+Or, if you used more than one Page, indicate it by using the following format:
+"Quellen: Seite Nummer, Seite Nummer"
+
+Antwort:    
+"""
+
+# NEW PDFs QA TEMPLATE
+
+new_pdf_qa_template ="""
+Role:
+You are a healthcare professional.
+ 
+Instructions: 
+Your task is to help healthcare professionals with the medical product descriptions provided below.
+Provide a detailed answer to the question only on the basis of the 4 Pages listed below.
+
+Question
+{question}
+
+---------
+Page {metadata01}:\n
+{doc01}
+
+Page {metadata02}:\n
+{doc02}
+
+Page {metadata03}:\n
+{doc03}
+
+Page {metadata04}:\n
+{doc04}
+---------
+
+Task 1: Your task is to help healthcare professionals with the medical product descriptions provided above.
+Provide a detailed answer to the question only on the basis of the 4 Pages listed above.
+Your answer must be in the German language.
+
+Task 2: Only after providing a detailed answer, indicate the Page or Pages in which you found the information to answer the question\
+only by using the following format:
+"Quellen: Seite Nummer"
+Or, if you used more than one Chapter, indicate it by using the following format:
+"Quellen: Seite Nummer, Seite Nummer"
+
+--------
+Here is an example of a Question and an Answer:
+
+Question:
+Was sind die Zulassungsdaten?
+
+Chapter 7:
+INHABER DER ZULASSUNG Bioprojet Pharma 9 rue Rameau 75002 Paris Frankreich Örtlicher Vertreter Bioprojet Deutschland GmbH, Bismarckstr. 63, 12169 Berlin Tel.:  030/3465 5460-0 E-Mail: info@bioprojet.de Medizinische Information Tel.:  0251/60935 599 E-Mail: arzneimittelnebenwirkung@bioprojet.com  8. ZULASSUNGSNUMMER 55388.00.00  9. DATUM DER ERTEILUNG DER ZULASSUNG/VERLÄNGERUNG DER ZULASSUNG 20. Dezember 2002/11. Juli 2006  10. STAND DER INFORMATION Januar 2019  11. VERKAUFSABGRENZUNG Verschreibungspflichtig
+
+Answer:
+Zulassungsdaten:
+- Inhaber der Zulassung: Bioprojet Pharma
+- Adresse des Inhabers der Zulassung: 9 rue Rameau, 75002 Paris, Frankreich
+- Örtlicher Vertreter: Bioprojet Deutschland GmbH, Bismarckstr. 63, 12169 Berlin
+- Zulassungsnummer: 55388.00.00
+- Datum der Erteilung der Zulassung: 20. Dezember 2002/11. Juli 2006
+- Stand der Information: Januar 2019
+- Verkaufabgrenzung: Verschreibungspflichtig
+
+Quellen: Kapitel 7
+--------
+
+Antwort:    
+""" 
